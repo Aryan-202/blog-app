cc0b9de7f336d07cbe29985b86375682bba2fbef
Aryan Vishwakarma <aryanvishwakarma275@gmail.com>
2025-08-30 19:39:34 +0530

    fixed bugs

--- components/BlogItem.jsx
+++ components/BlogItem.jsx
@@ -2,14 +2,14 @@ import { assets, blog_data } from '@/assets/assets'
 import Image from 'next/image'
 import React from 'react'
 
-const BlogItem = () => {
+const BlogItem = ({image,title,category,description}) => {
   return (
     <div className='max-w-[300px] sm:max-w-[300px] bg-white border border-black hover:shadow-[-7px_7px_0px_#000000]'>
-      <Image src={blog_data[0].image} width={400} height={400} alt='Blog Image' className='border-b border-black'/>
-      <p className='ml-5 mt-5 px-1 inline-block bg-black text-white text-sm'>{blog_data[0].category}</p>
+      <Image src={image} width={400} height={400} alt='Blog Image' className='border-b border-black'/>
+      <p className='ml-5 mt-5 px-1 inline-block bg-black text-white text-sm'>{category}</p>
       <div className="p-5">
-        <h5 className='mb-2 font-medium text-lg tracking-tight text-gray-900'>{blog_data[0].title}</h5>
-        <p className='text-sm mb-3 tracking-tight text-gray-700'>{blog_data[0].description}</p>
+        <h5 className='mb-2 font-medium text-lg tracking-tight text-gray-900'>{title}</h5>
+        <p className='text-sm mb-3 tracking-tight text-gray-700'>{description}</p>
         <div className='inline-flex items-center py-2 font-semibold text-center'>
           Read more
           <Image src={assets.arrow} alt='Blog Image' width={12} className='ml-2' />
