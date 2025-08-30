66d46013c7c9ecdf44ff7dba6cc6d88a640fba24
Aryan Vishwakarma <aryanvishwakarma275@gmail.com>
2025-08-30 22:57:06 +0530

    fixed bugs related to params

---
+++ app/blogs/[id]/page.jsx
@@ -0,0 +1,31 @@
+"use client"
+import { blog_data } from '@/assets/assets';
+import React, { useEffect, useState } from 'react'
+
+const page = ({params}) => {
+
+    const id = React.use(params).id;
+    const [data, setData] = useState(null);
+
+    const fetchBlogData = () =>{
+        for(let i=0; i<blog_data.length; i++){
+            if(Number(id) === blog_data[i].id){
+                setData(blog_data[i]);
+                console.log(blog_data[i]);
+                break;
+            }
+        }
+    }
+
+    useEffect(()=>{
+        fetchBlogData();
+    },[])
+
+  return (
+    <div>
+      {id}
+    </div>
+  )
+}
+
+export default page
--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@
 import BlogList from "@/components/BlogList";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
-import Image from "next/image";
+
 
 export default function Home() {
   return (
