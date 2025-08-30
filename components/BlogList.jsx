5c514a7f9c637ad6299ba616ffa50bd8586e3d9e
Aryan Vishwakarma <aryanvishwakarma275@gmail.com>
2025-08-30 19:59:10 +0530

    added logic in blog list

--- components/BlogList.jsx
+++ components/BlogList.jsx
@@ -1,18 +1,21 @@
 import { blog_data } from '@/assets/assets'
-import React from 'react'
+import React, { useState } from 'react'
 import BlogItem from './BlogItem'
 
 const BlogList = () => {
+
+    const [menu, setMenu] = useState("All")
+
   return (
     <div>
       <div className="justify-center gap-6 flex my-10 ">
-        <button className='text-white  bg-black py-1 px-4 rounded-sm'>All</button>
-        <button className='text-white  bg-black py-1 px-4 rounded-sm'>Technology</button>
-        <button className='text-white  bg-black py-1 px-4 rounded-sm'>Startup</button>
-        <button className='text-white  bg-black py-1 px-4 rounded-sm'>Lifestyle</button>
+        <button onClick={() => setMenu("All")} className={menu==="All"?'text-white  bg-black py-1 px-4 rounded-sm':""}>All</button>
+        <button onClick={() => setMenu("Technology")} className={menu==="Technology"?'text-white  bg-black py-1 px-4 rounded-sm':""}>Technology</button>
+        <button onClick={() => setMenu("Startup")} className={menu==="Startup"?'text-white  bg-black py-1 px-4 rounded-sm':""}>Startup</button>
+        <button onClick={() => setMenu("Lifestyle")} className={menu==="Lifestyle"?'text-white  bg-black py-1 px-4 rounded-sm':""}>Lifestyle</button>
       </div>
       <div className='flex flex-wrap justify-around gap-1 gap-y-10 mb-16 xl:mx-24 '>
-        {blog_data.map((item,index)=>{
+        {blog_data.filter((item) => menu === "All"? true : item.category === menu).map((item,index)=>{
             return <BlogItem key={index} image={item.image} title={item.title} category={item.category} description={item.description} />
         })}
       </div>
