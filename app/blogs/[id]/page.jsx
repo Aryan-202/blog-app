b6bd58765d40a5b0828fa8a1701c00d1ce86ed83
Aryan Vishwakarma <aryanvishwakarma275@gmail.com>
2025-08-30 23:50:38 +0530

    loram ipsom

--- app/blogs/[id]/page.jsx
+++ app/blogs/[id]/page.jsx
@@ -34,8 +34,16 @@ const page = ({params}) => {
         </div>
         <div className='text-center my-24'>
             <h1 className='text-2xl sm:text-5xl font-semibold max-w-700px mx-auto'>{data?.title}</h1>
-            <Image src={data?.author_img} alt='' width={60} height={60} className='mx-auto mt-10'/>
+            <Image src={data?.author_img} alt='' width={60} height={60} className='mx-auto mt-6 border border-white rounded-full'/>
+            <p className='mt-1 pb-2 text-lg max-w-[740px] mx-auto'>{data?.author}</p>
         </div>
+    </div>
+    <div className='mx-5 max-w-[800px] md:mx-auto mt-[-100px] mb-10'>
+        <Image src={data?.image} alt='' width={1280} height={720} className='border-4 border-white'/>
+        <h1 className='my-8 text-[26px] font-semibold'>Introduction:</h1>
+        <p>{data?.description}</p>
+        <h3 className='my-5 text-[18px] font-semibold'>Key Points: some key points here to show</h3>
+        <p className='my-3 '>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, accusantium? Dolorem facere porro ullam fugiat consequuntur dolores repudiandae modi excepturi dignissimos sint, molestiae fugit, iure, deserunt reiciendis qui alias voluptates!</p>
     </div>
     </>)
     :<div className='text-center my-24'>
